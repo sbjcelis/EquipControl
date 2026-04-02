f0ac8f4ea68dcb98d168b5703db58bad1dcb32fb
sbjcelis <jcelis@southbound.travel>
2026-04-02 18:09:58 -0300

    Issues varios

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Inventario Equipos Computacionales</title>
+  <title>Equipos Soutbound</title>
   <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='75' font-size='75'>💻</text></svg>">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
